aeac4f0aa9857dc4ff317cbe85783263d0918fa6
jorgelaciarts <jorgelaciarts@gmail.com>
2026-07-09 22:18:26 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// ============================================================
+// CONFIGURACIÓN DE FIREBASE — Liceo Almirante Riveros
+// ============================================================
+// Proyecto: liceo-almirante-riveros
+// ============================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHoFA_4ORK_Bzc0JZTJ-0uuBA61hu9Sx4",
+  authDomain: "liceo-almirante-riveros.firebaseapp.com",
+  projectId: "liceo-almirante-riveros",
+  storageBucket: "liceo-almirante-riveros.firebasestorage.app",
+  messagingSenderId: "250920391524",
+  appId: "1:250920391524:web:468b850106038aad896071",
+  measurementId: "G-ZG514LK4W3"
+};
